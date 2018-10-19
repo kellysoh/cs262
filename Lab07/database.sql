de6aa8b54184ede9a11b30611c335bf99a1fc934
Seeun Oh <so37@gold03.cs.calvin.edu>
2018-10-19 18:42:28 -0400

    Update Lab07

--- Lab07/database.sql
+++ Lab07/database.sql
@@ -7,9 +7,12 @@
 
 -- Drop previous versions of the tables if they they exist, in reverse order of foreign keys.
 DROP TABLE IF EXISTS PlayerGame;
+DROP TABLE IF EXISTS PlayingGame;
+DROP TABLE IF EXISTS Property;
 DROP TABLE IF EXISTS Game;
 DROP TABLE IF EXISTS Player;
-DROP TABLE IF EXISTS PlayingGame;
+
+
 
 -- Create the schema.
 CREATE TABLE Game (
@@ -34,16 +37,23 @@ CREATE TABLE PlayingGame (
     gameID integer REFERENCES Game(ID),
     playerID integer REFERENCES Player(ID),
     cash integer,
-    property varchar(50),
-    house integer,
-    hotel integer,
     location varchar(50)
 );
+
+CREATE TABLE Property (
+	gameID integer REFERENCES Game(ID),
+	playerID integer REFERENCES Player(ID),
+	property varchar(50),
+	house integer CHECK (house <=4),
+	hotel boolean
+); 
+
 -- Allow users to select data from the tables.
 GRANT SELECT ON Game TO PUBLIC;
 GRANT SELECT ON Player TO PUBLIC;
 GRANT SELECT ON PlayerGame TO PUBLIC;
 GRANT SELECT ON PlayingGame TO PUBLIC;
+GRANT SELECT ON Property TO PUBLIC; 
 
 -- Add sample records.
 INSERT INTO Game VALUES (1, '2006-06-27 08:00:00', '2006-06-27 10:00:00');
@@ -64,11 +74,19 @@ INSERT INTO PlayerGame VALUES (2, 3, 500.00);
 INSERT INTO PlayerGame VALUES (3, 2, 0.00);
 INSERT INTO PlayerGame VALUES (3, 3, 5500.00);
 
-INSERT INTO PlayingGame VALUES (4, 1, 100, 'SEOUL', 2, 1 , 'GRAND RAPIDS');
-INSERT INTO PlayingGame VALUES (4, 2, 1000, 'ULAANBAATAR', 10, 20, 'RUSSIA');
+INSERT INTO PlayingGame VALUES (4, 1, 100, 'GRAND RAPIDS');
+INSERT INTO PlayingGame VALUES (4, 2, 1000,  'RUSSIA');
+
+INSERT INTO Property VALUES (4, 1, 'SEOUL', 2, TRUE);
+INSERT INTO Property VALUES (4, 1, 'HELSINKI', 3, FALSE); 
+INSERT INTO Property VALUES (4, 2, 'ULAANBAATAR', 4, TRUE );
 
 SELECT * FROM Game;
 SELECT * FROM PlayerGame;
 SELECT * FROM PlayingGame;
 SELECT COUNT(*) AS PLAYER_COUNT FROM Player;
 SELECT * FROM Player;
+SELECT *  FROM PlayingGame a 
+INNER JOIN Property b
+ON a.GameID = b.GameID 
+AND a.PlayerID = b.PlayerID; 
